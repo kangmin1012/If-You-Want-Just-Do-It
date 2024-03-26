7afe311add17cc111371b1f619683701edc1b131
kangmin1012 <kangmin1012@naver.com>
2024-03-26 16:18:53 +0900

    생년월일 설정 바텀시트 레이아웃 추가

--- feature/userinfo/src/main/java/kang/min/userinfo/ui/BirthdayInfoScreen.kt
+++ feature/userinfo/src/main/java/kang/min/userinfo/ui/BirthdayInfoScreen.kt
@@ -4,14 +4,24 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -19,13 +29,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ifyouwant.justdo.ui.IfYouWantTheme
 import kang.min.userinfo.R
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BirthdayInfoScreen() {
 
+    val coroutineScope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
-            modifier = Modifier.align(Alignment.TopCenter).padding(top = 100.dp),
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 100.dp),
             painter = painterResource(id = R.drawable.img_birthday_cake),
             contentDescription = "생일 타이틀 이미지"
         )
@@ -42,9 +60,32 @@ fun BirthdayInfoScreen() {
             Text(text = "나이 : NN살", style = MaterialTheme.typography.bodyLarge)
             Spacer(modifier = Modifier.height(32.dp))
             Button(
-                onClick = { }
+                onClick = { showBottomSheet = true }
             ) {
                 Text(text = "선택하기")
+            }
+        }
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = { showBottomSheet = false },
+                sheetState = sheetState
+            ) {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    onClick = {
+                        coroutineScope.launch { sheetState.hide() }.invokeOnCompletion {
+                            showBottomSheet = false
+                        }
+                    },
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    Text(text = "선택")
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
     }
